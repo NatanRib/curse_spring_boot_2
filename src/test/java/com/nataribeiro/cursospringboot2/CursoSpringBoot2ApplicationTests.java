e51c18a72260e8cf6476e6baf32b96a7cd9d3722
Natan Ribeiro <ntnmarcal@gmail.com>
2021-05-17 14:51:05 -0300

    Project created

---
+++ src/test/java/com/nataribeiro/cursospringboot2/CursoSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataribeiro.cursospringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
